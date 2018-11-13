c04d956b2df511bdb9dc4421c4d8a3b4c2cdd400
Tatev <ttevik@gmail.com>
2018-11-13 15:19:39 +0100

    2 modals added. plan page added

--- script.js
+++ script.js
@@ -6,3 +6,25 @@ function parallax() {
   var scrolled = $(window).scrollTop();
   $(".background").css("top", -(scrolled * 0.15) + "px");
 }
+
+$("#formModal").on("shown.bs.modal", function() {
+  $("#inputName").trigger("focus");
+});
+
+$("#datepicker").datepicker({
+  format: "yyyy-mm-dd"
+});
+
+function highlightCard() {
+  $(".card").removeClass("highlight");
+  let targetId;
+  if (event.target.tagName === "g") {
+    targetId = event.target.id + "_card";
+  } else {
+    targetId =
+      $(event.target)
+        .parents("g")
+        .attr("id") + "_card";
+  }
+  document.getElementById(targetId).classList.add("highlight");
+}
